b9c06df73cdae6968fa4a019da948678b4ad52f3
mantinhas <mantinhas@github.com>
2024-08-11 04:17:06 +0100

    Added Cobra CLI confed get command and flags skeleton

---
+++ cmd/get.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get {key}",
+	Short: "Get the value from a key",
+	Args:  cobra.ExactArgs(1),
+	Long: `Get the value corresponding to a key in the TOML input file.
+Example:
+	confed get name`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("key", "", "Key from the attribute to get value from")
+	// getCmd.MarkPersistentFlagRequired("key")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/mantinhas/confed/cmd"
+
+func main() {
+	cmd.Execute()
+}
